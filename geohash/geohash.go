3acec526a69c041c34845fefa0ed886bbdcf1709
vargi <tunavargi@gmail.com>
2016-04-17 18:29:17 +0300

    init commit

---
+++ geohash/geohash.go
@@ -0,0 +1,217 @@
+package geohash
+
+import (
+	"bytes"
+	"strings"
+)
+
+var (
+	bits      = []int{16, 8, 4, 2, 1}
+	base32    = []byte("0123456789bcdefghjkmnpqrstuvwxyz")
+	neighbors = [][]string{
+		[]string{
+			"p0r21436x8zb9dcf5h7kjnmqesgutwvy",
+			"bc01fg45238967deuvhjyznpkmstqrwx",
+		},
+		[]string{
+			"bc01fg45238967deuvhjyznpkmstqrwx",
+			"p0r21436x8zb9dcf5h7kjnmqesgutwvy",
+		},
+		[]string{
+			"14365h7k9dcfesgujnmqp0r2twvyx8zb",
+			"238967debc01fg45kmstqrwxuvhjyznp",
+		},
+		[]string{
+			"238967debc01fg45kmstqrwxuvhjyznp",
+			"14365h7k9dcfesgujnmqp0r2twvyx8zb",
+		},
+	}
+	borders = [][]string{
+		[]string{
+			"prxz",
+			"bcfguvyz",
+		},
+		[]string{
+			"bcfguvyz",
+			"prxz",
+		},
+		[]string{
+			"028b",
+			"0145hjnp",
+		},
+		[]string{
+			"0145hjnp",
+			"028b",
+		},
+	}
+)
+
+// Calculates adjacent geohashes.
+func CalculateAdjacent(s, dir string) string {
+	s = strings.ToLower(s)
+	lastChr := s[(len(s) - 1):]
+	oddEven := (len(s) % 2) // 0=even; 1=odd;
+	var dirInt int
+	switch dir {
+	default:
+		dirInt = 0
+	case "right":
+		dirInt = 1
+	case "bottom":
+		dirInt = 2
+	case "left":
+		dirInt = 3
+	}
+	// base := s[0:]
+	base := s[:(len(s) - 1)]
+	if strings.Index(borders[dirInt][oddEven], lastChr) != -1 {
+		base = CalculateAdjacent(base, dir)
+	}
+	return base + string(base32[strings.Index(neighbors[dirInt][oddEven], lastChr)])
+}
+
+// Shortcut to calculate all adjacent geohashes.
+func CalculateAllAdjacent(s string) []string {
+	values := []string{}
+	directions := []string{"top", "right", "bottom", "left"}
+	for _, direction := range directions {
+		neighbour := CalculateAdjacent(s, direction)
+		values = append(values, neighbour)
+		if direction == "top" || direction == "bottom" {
+			values = append(values, CalculateAdjacent(neighbour, "right"))
+			values = append(values, CalculateAdjacent(neighbour, "left"))
+		}
+	}
+	return values
+}
+
+// Struct for passing Box.
+type BoundingBox struct {
+	sw     LatLng
+	ne     LatLng
+	center LatLng
+}
+
+// Returns coordinates for box's center.
+func (b *BoundingBox) Center() *LatLng {
+	return &b.center
+}
+
+// Returns coordinates for box's South-West corner.
+func (b *BoundingBox) SouthWest() *LatLng {
+	return &b.sw
+}
+
+// Returns coordinates for box's North-East corner.
+func (b *BoundingBox) NorthEast() *LatLng {
+	return &b.ne
+}
+
+// Struct for passing LatLng values.
+type LatLng struct {
+	lat float64
+	lng float64
+}
+
+// Returns latitude.
+func (ll *LatLng) Lat() float64 {
+	return ll.lat
+}
+
+// Returns longitude.
+func (ll *LatLng) Lng() float64 {
+	return ll.lng
+}
+
+func refineInterval(interval []float64, cd, mask int) []float64 {
+	if cd&mask > 0 {
+		interval[0] = (interval[0] + interval[1]) / 2
+	} else {
+		interval[1] = (interval[0] + interval[1]) / 2
+	}
+	return interval
+}
+
+// Get LatLng coordinates from a geohash
+func Decode(geohash string) *BoundingBox {
+	isEven := true
+	lat := []float64{-90, 90}
+	lng := []float64{-180, 180}
+	latErr := float64(90)
+	lngErr := float64(180)
+	var c string
+	var cd int
+	for i := 0; i < len(geohash); i++ {
+		c = geohash[i : i+1]
+		cd = bytes.Index(base32, []byte(c))
+		for j := 0; j < 5; j++ {
+			if isEven {
+				lngErr /= 2
+				lng = refineInterval(lng, cd, bits[j])
+			} else {
+				latErr /= 2
+				lat = refineInterval(lat, cd, bits[j])
+			}
+			isEven = !isEven
+		}
+	}
+	return &BoundingBox{
+		sw: LatLng{
+			lat: lat[0],
+			lng: lng[0],
+		},
+		ne: LatLng{
+			lat: lat[1],
+			lng: lng[1],
+		},
+		center: LatLng{
+			lat: (lat[0] + lat[1]) / 2,
+			lng: (lng[0] + lng[1]) / 2,
+		},
+	}
+}
+
+// Create a geohash with 12 positions based on LatLng coordinates
+func Encode(latitude, longitude float64) string {
+	return EncodeWithPrecision(latitude, longitude, 12)
+}
+
+// Create a geohash with given precision (number of characters of the resulting
+// hash) based on LatLng coordinates
+func EncodeWithPrecision(latitude, longitude float64, precision int) string {
+	isEven := true
+	lat := []float64{-90, 90}
+	lng := []float64{-180, 180}
+	bit := 0
+	ch := 0
+	var geohash bytes.Buffer
+	var mid float64
+	for geohash.Len() < precision {
+		if isEven {
+			mid = (lng[0] + lng[1]) / 2
+			if longitude > mid {
+				ch |= bits[bit]
+				lng[0] = mid
+			} else {
+				lng[1] = mid
+			}
+		} else {
+			mid = (lat[0] + lat[1]) / 2
+			if latitude > mid {
+				ch |= bits[bit]
+				lat[0] = mid
+			} else {
+				lat[1] = mid
+			}
+		}
+		isEven = !isEven
+		if bit < 4 {
+			bit++
+		} else {
+			geohash.WriteByte(base32[ch])
+			bit = 0
+			ch = 0
+		}
+	}
+	return geohash.String()
+}
---
+++ main.go
@@ -0,0 +1,136 @@
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import (
+	"flag"
+	"log"
+	"io/ioutil"
+	"net/http"
+	"html/template"
+	"github.com/garyburd/redigo/redis"
+	"github.com/gorilla/websocket"
+	"github.com/gorilla/mux"
+	"github.com/vargi/geopublisher/geohash"
+	"fmt"
+	"strconv"
+	"net/url"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{} // use default options
+
+
+func getGeoHash(vars url.Values)(string){
+	lat := vars.Get("lat")
+	if !(len(lat) >  0) {
+		return ""
+	}
+	lng := vars.Get("lng")
+	if !(len(lng) >  0) {
+		return ""
+	}
+	precision := vars.Get("precision")
+	if !(len(precision) >  0) {
+		return ""
+	}
+	channel := vars.Get("channel_id")
+	if !(len(channel) >  0) {
+		return ""
+	}
+	xlat, err := strconv.Atoi(lat)
+	xlng, err := strconv.Atoi(lng)
+	xprecision, err := strconv.Atoi(precision)
+	if err != nil {
+		return ""
+	}
+
+	geohash := geohash.EncodeWithPrecision(float64(xlat), float64(xlng), xprecision)
+	geohash = channel + "|" + geohash
+	return geohash
+}
+
+
+
+
+func postMessageHandler(w http.ResponseWriter, r *http.Request) {
+	redis_conn, err := redis.Dial("tcp", ":6379")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	vars := r.URL.Query()
+	geohash := getGeoHash(vars)
+	if !(len(geohash) > 0) {
+		return
+	}
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(geohash)
+	redis_conn.Do("PUBLISH", geohash, string(body))
+}
+
+
+func socketHandler(w http.ResponseWriter, r *http.Request) {
+	redis_conn , err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		panic(err)
+	}
+
+	psc := redis.PubSubConn{redis_conn}
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	vars := r.URL.Query()
+
+	geohash := getGeoHash(vars)
+	if !(len(geohash) > 0) {
+		return
+	}
+	geohash = geohash + "*"
+	err = psc.PSubscribe(geohash)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("hello")
+	for {
+	    switch v := psc.Receive().(type) {
+	    case redis.PMessage:
+	        fmt.Println("hello")
+		fmt.Printf("%s: message: %s\n", v.Channel, v.Data)
+		err = c.WriteMessage(1, v.Data)
+	        if err != nil {
+			panic(err)
+		}
+
+	    case redis.Subscription:
+		fmt.Printf("%s: %s %d\n", v.Channel, v.Kind, v.Count)
+	    }
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+		homeTemplate.Execute(w, "ws://"+r.Host+"/socket?lat=47&lng=120&precision=5&channel_id=xyz")
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+	r := mux.NewRouter()
+	r.HandleFunc("/socket", socketHandler)
+	r.HandleFunc("/messages", postMessageHandler)
+	r.HandleFunc("/", home)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
+
+}
